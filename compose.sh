cbd88c5dc8995c760558da5ecc06b0a14ed9545f
peskaf <filip@peska.me>
2024-03-01 11:40:29 +0100

    Added HTTPS for prod mode.

--- compose.sh
+++ compose.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -eu
 
+# Function to extract specific environment variable from .env file
+get_env_variable() {
+    local env_file="$1"
+    local variable_name="$2"
+    local value
+
+    # Check if .env file exists
+    if [[ -f "$env_file" ]]; then
+        # Extract the value of the specified variable
+        value=$(grep -E "^$variable_name=" "$env_file" | cut -d '=' -f 2-)
+        echo "$value"
+    fi
+}
+
+# Load the PROD_SERVER_NAME environment variable from .env file
+PROD_SERVER_NAME=$(get_env_variable ".env" "PROD_SERVER_NAME")
+
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -24,6 +41,14 @@ if [ "$MODE" = "dev" ]; then
     DOCKER_COMPOSE_EXTENSION="docker-compose.dev.yml"
 elif [ "$MODE" = "prod" ]; then
     DOCKER_COMPOSE_EXTENSION="docker-compose.prod.yml"
+    if [ ! -f "$SCRIPT_DIR/certbot/conf/live/${PROD_SERVER_NAME}/fullchain.pem;" ] && [ ! -f "$SCRIPT_DIR/certbot/conf/live/${PROD_SERVER_NAME}/privkey.pem;" ]; then
+        # TODO: test
+        # Run Certbot to generate certificates
+        docker compose -f "$SCRIPT_DIR/docker-compose.yml" -f "$SCRIPT_DIR/$DOCKER_COMPOSE_EXTENSION" run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d $PROD_SERVER_NAME
+        # docker compose -f "$SCRIPT_DIR/docker-compose.yml" -f "$SCRIPT_DIR/$DOCKER_COMPOSE_EXTENSION" restart
+        docker compose -f "$SCRIPT_DIR/docker-compose.yml" -f "$SCRIPT_DIR/$DOCKER_COMPOSE_EXTENSION" down --remove-orphans
+        echo "Certificates successfully generated."
+    fi
 else
     echo "Invalid mode. Please specify 'dev' or 'prod'."
     exit 1
